84510ec653707d2f000beaffdf9642155fc3d5f7
Ratchanan Srirattanamet <ratchanan@ubports.com>
2021-06-16 02:35:50 +0700

    src: create an exporting index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import RTCDataChannel from './webrtc/RTCDataChannel';
+import RTCIceCandidate from './webrtc/RTCIceCandidate';
+import RTCPeerConnection from './webrtc/RTCPeerConnection';
+import RTCSessionDescription from './webrtc/RTCSessionDescription';
+
+export {
+    RTCDataChannel,
+    RTCIceCandidate,
+    RTCPeerConnection,
+    RTCSessionDescription,
+};
